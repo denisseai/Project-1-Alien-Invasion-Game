87b8e4849529bfc2ba66be47dd7e847e4120f021
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-22 14:58:24 -0800

    Fleet moves right and disappears

--- game_functions.py
+++ game_functions.py
@@ -97,3 +97,6 @@ def create_fleet(ai_settings, screen, ship, aliens):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number,
                             row_number)
+def update_aliens(aliens):
+    """Update teh position of all aliens in the fleet"""
+    aliens.update()
