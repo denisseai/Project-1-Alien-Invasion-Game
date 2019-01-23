ca41c9064fe91d5d4d0368a8ba889f531382ab18
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-22 18:01:52 -0800

    Shooting aliens

--- game_functions.py
+++ game_functions.py
@@ -52,7 +52,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     #Make the most recently drawn screen visible
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """Update position of bullets and get rid of old bullets"""
     #Update bullet positions
     bullets.update()
@@ -60,6 +60,9 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    #Check for any bullets that have hit aliens
+    #If so, get rid of the bulllet and the alien
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row"""
