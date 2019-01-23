f83dbeb64e1ec02e3b7cc6f09e1dae2fa17ef9a1
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-22 18:17:12 -0800

    Creates a new fleet after old one is destroyed

--- game_functions.py
+++ game_functions.py
@@ -52,7 +52,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     #Make the most recently drawn screen visible
     pygame.display.flip()
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets"""
     #Update bullet positions
     bullets.update()
@@ -63,6 +63,11 @@ def update_bullets(aliens, bullets):
     #Check for any bullets that have hit aliens
     #If so, get rid of the bulllet and the alien
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        #Destroy existing bullets and create new fleet
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row"""
