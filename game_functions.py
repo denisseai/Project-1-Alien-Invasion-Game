85be8729f1fe64c2f976bbf4dc131f9862067854
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-24 15:17:02 -0800

    Active "Play" button

--- game_functions.py
+++ game_functions.py
@@ -24,15 +24,23 @@ def check_key_up_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
-  """Respond to keypresses and mouse events"""
-  for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-         sys.exit()
-      elif  event.type == pygame.KEYDOWN:
-         check_keydown_events(event, ai_settings, screen, ship, bullets)
-      elif event.type == pygame.KEYUP:
-         check_key_up_events(event, ship)
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+    """Respond to keypresses and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif  event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_key_up_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Start a new game when the player clicks Play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit is not reached yet"""
