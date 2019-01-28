9c0143a7c4483bd7685ed3772230b0a66c571cf5
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-27 17:09:44 -0800

    Active scoreboard

--- game_functions.py
+++ game_functions.py
@@ -81,7 +81,7 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
 
     #Draw the score information
     sb.show_score()
-    
+
     #Draw the play button if the game is inactive
     if not stats.game_active:
         play_button.draw_button()
@@ -89,7 +89,7 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
     #Make the most recently drawn screen visible
     pygame.display.flip()
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets"""
     #Update bullet positions
     bullets.update()
@@ -99,12 +99,18 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
+                                    aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
+                                    aliens, bullets):
     """Respond to bullet-alien collisions"""
     #Remove any bullets and aliens that have collided
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if collisions:
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         #Destroy existing bullets, speed up game, and create new fleet
