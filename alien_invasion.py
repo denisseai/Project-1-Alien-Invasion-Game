7ed9f665f562e8968ec4d223ed2e4d3000a36f8e
Denisse Anaya <msdenisseanaya@gmail.com>
2019-01-28 17:56:05 -0800

    Finished project

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,16 +36,14 @@ def run_game():
 
   #Start the main loop for the game
   while True:
-      gf.check_events(ai_settings, screen, stats, play_button, ship,
+      gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
                         aliens, bullets)
       if stats.game_active:
           ship.update()
           gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
-                                bullets)
-          gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-
-      """gf.update_screen(ai_settings, screen, ship, aliens, bullets)"""
-
+                            bullets)
+          gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                            bullets)
       #Get rid of bullets that have disappeared
       for bullet in bullets.copy():
           if bullet.rect.bottom < 0:
--- game_stats.py
+++ game_stats.py
@@ -9,10 +9,14 @@ class GameStats():
     #Start game in inactive state
     self.game_active = False
 
+    #High score should never be reset
+    self.high_score = 0
+
   def reset_stats(self):
     """Initialize statistics that can change during game"""
     self.ships_left = self.ai_settings.ship_limit
     self.score = 0
+    self.level = 1
 
     #Start Alien Invasion in an active state
     self.game_active = True
